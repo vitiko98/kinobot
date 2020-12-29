9a91a6ddb6abdeb6ea2c8937f5ad2c696a1ac4f0
vitiko98 <averroista@protonmail.com>
2020-12-29 19:58:49 -0400

    Add NSFW detection; add discord bot and notifications

--- kinobot/config.py
+++
@@ -1,8 +0,0 @@
-#if datetime.now().strftime("%M") not in PUBLISH_MINUTES:
-#    # /scripts/backup_for_test.sh
-#    KINOBASE = KINOBASE + ".save"
-#    REQUESTS_JSON = REQUESTS_JSON + ".save"
-#    REQUESTS_DB = REQUESTS_DB + ".save"
-#    logger.warning("Using temporal databases")
-#else:
-#    logger.warning("Using official databases")
