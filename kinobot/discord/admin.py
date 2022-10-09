d0f7a3da0797df447f8ed44ad6fa734a2692bb73
Vitiko <averroista@protonmail.com>
2022-10-09 11:25:07 -0400

    Update discord bot role permissions

--- kinobot/discord/admin.py
+++ kinobot/discord/admin.py
@@ -100,7 +100,7 @@ async def chamber(ctx: commands.Context, *args):
             await _gkey(ctx, 1.0, member_id, "From chamber")
 
 
-@commands.has_any_role("botmin", "certified verifier")
+@commands.has_any_role("botmin", "verifier")
 @bot.command(name="schamber", help="Enter the verification chamber.")
 async def schamber(ctx: commands.Context):
     chamber = Chamber(bot, ctx)
