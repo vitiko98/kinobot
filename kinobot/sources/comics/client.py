8f0cf067f89e4b2742120e55efd60f7561c66b5a
Vitiko <averroista@protonmail.com>
2023-06-06 00:12:41 -0400

    Add comics support

---
+++ kinobot/sources/comics/client.py
@@ -0,0 +1,152 @@
+import re
+from typing import List, Optional
+
+from kinobot.utils import get_yaml_config
+from kinobot.constants import YAML_CONFIG
+
+from pydantic import BaseModel
+import requests
+
+_CHAPTER_RE = re.compile(r"(chapter|issue)\s(?P<x>[\d\S]+)", flags=re.IGNORECASE)
+_PAGE_RE = re.compile(r"page\s(?P<x>\d+)", flags=re.IGNORECASE)
+_ID_RE = re.compile(r"id:\s?(?P<x>\d+)", flags=re.IGNORECASE)
+
+
+class ComicQuery(BaseModel):
+    title: Optional[str] = None
+    id: Optional[str] = None
+    chapter: Optional[int] = None
+    page: Optional[int] = None
+
+    @classmethod
+    def from_str(cls, str_: str):
+        try:
+            id_ = _ID_RE.search(str_).group("x")
+        except (AttributeError, IndexError):
+            id_ = None
+
+        try:
+            chapter = _CHAPTER_RE.search(str_).group("x")
+        except (AttributeError, IndexError):
+            chapter = None
+
+        try:
+            page = _PAGE_RE.search(str_).group("x")
+        except (AttributeError, IndexError):
+            page = None
+
+        title = str_
+        for to_r in (_CHAPTER_RE, _PAGE_RE, _ID_RE):
+            title = to_r.sub("", title).strip()
+
+        return cls(title=title, id=id_, chapter=chapter, page=page)  # type: ignore
+
+
+def _to_camel(string: str) -> str:
+    result = "".join(word.capitalize() for word in string.split("_"))
+    return result[0].lower() + result[1:]
+
+
+class IdName(BaseModel):
+    id: int
+    name: str
+
+
+class SeriesMetadata(BaseModel):
+    id: int
+    writers: List[IdName] = []
+    publishers: List[IdName] = []
+    editors: List[IdName] = []
+    colorists: List[IdName] = []
+    language: str = "en"
+    release_year: str
+
+    class Config:
+        alias_generator = _to_camel
+
+
+class Chapter(BaseModel):
+    id: int
+    number: str = ""
+    title = ""
+    pages: int
+
+
+class Series(BaseModel):
+    id: int
+    name: str
+    metadata: Optional[SeriesMetadata] = None
+    chapters: List[Chapter] = []
+
+
+class Client:
+    def __init__(self, url, authorization, api_key, session=None) -> None:
+        self._url = url
+        self._api_key = api_key
+        self._session = session or requests.Session()
+        headers = {
+            "Accept": "application/json",
+            "Authorization": authorization,
+        }
+        self._session.headers.update(headers)
+
+    @classmethod
+    def from_config(cls, path=None):
+        return cls(**get_yaml_config(path or YAML_CONFIG, "comics"))
+
+    def search(self, query):
+        params = {
+            "queryString": query,
+        }
+        response = self._session.get(f"{self._url}/api/search/search", params=params)
+        response.raise_for_status()
+        return [
+            Series(id=item["seriesId"], name=item["name"])
+            for item in response.json()["series"]
+        ]
+
+    def series_metadata(self, id):
+        params = {
+            "seriesId": id,
+        }
+
+        response = self._session.get(f"{self._url}/api/series/metadata", params=params)
+        response.raise_for_status()
+        return SeriesMetadata(**response.json())
+
+    def series_chapters(self, id):
+        params = {
+            "seriesId": id,
+        }
+
+        response = self._session.get(
+            f"{self._url}/api/series/series-detail", params=params
+        )
+        response.raise_for_status()
+        return [Chapter(**item) for item in response.json()["chapters"]]
+
+    def image_url(self, chapter_id, page):
+        return f"{self._url}/api/reader/image?chapterId={chapter_id}&apiKey={self._api_key}&page={page}"
+
+    def first_series_matching(self, query):
+        result = self.search(query)
+        if not result:
+            return None
+
+        item = result[0]
+        item.metadata = self.series_metadata(item.id)
+        item.chapters = self.series_chapters(item.id)
+
+        return item
+
+    def series(self, id):
+        response = self._session.get(f"{self._url}/api/series/{id}")
+        response.raise_for_status()
+        return Series(**response.json())
+
+    def get_series(self, id):
+        item = self.series(id)
+        item.metadata = self.series_metadata(item.id)
+        item.chapters = self.series_chapters(item.id)
+
+        return item
---
+++ kinobot/sources/comics/extractor.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+
+import hashlib
+import logging
+
+from kinobot import exceptions
+
+from . import client as comic_client
+from .. import utils
+from ..abstract import AbstractMedia
+from .client import ComicQuery
+
+logger = logging.getLogger(__name__)
+
+
+class ComicPage(AbstractMedia):
+    type = "comic"
+
+    def __init__(
+        self, uri, model: comic_client.Series, chapter_model: comic_client.Chapter
+    ):
+        self._uri = uri
+        self._stream = None
+        self._chapter_model = chapter_model
+        self._model = model
+
+    @classmethod
+    def from_request(cls, query: str):
+        if f"!comic" in query:
+            return cls
+
+        return None
+
+    @property
+    def id(self):
+        return hashlib.md5(self._uri.encode("utf-8")).hexdigest()
+
+    @property
+    def path(self):
+        return self.id
+
+    @property
+    def pretty_title(self) -> str:
+        title = self.simple_title
+
+        if self._model.metadata is not None:
+            publishers = self._model.metadata.publishers
+            if publishers:
+                publishers = _grammatically_join([item.name for item in publishers])
+                title = f"{title}\n{publishers}"
+
+        return title
+
+    @property
+    def markdown_url(self) -> str:
+        raise NotImplementedError
+
+    @property
+    def simple_title(self) -> str:
+        title = self._model.name
+        if self._chapter_model.title:
+            title = f"{title} [{self._chapter_model.title}]"
+        elif self._chapter_model.number:
+            title = f"{title} [#{self._chapter_model.number}]"
+
+        return title
+
+    @property
+    def parallel_title(self) -> str:
+        return self.simple_title
+
+    @property
+    def metadata(self):
+        return None
+
+    @classmethod
+    def from_id(cls, id_):
+        return cls.from_query(id_)  # fixme
+
+    @classmethod
+    def from_query(cls, query: str, client=None):
+        query_ = ComicQuery.from_str(query)
+        if query_.page is None or query_.chapter is None:
+            raise exceptions.InvalidRequest(
+                "Chapter/Issue and page numbers are required (COMIC CHAPTER 1 PAGE 1)"
+            )
+
+        logger.debug("Query: %s", query_)
+
+        client = client or comic_client.Client.from_config()
+
+        if query_.id is not None:
+            comic = client.get_series(query_.id)
+        else:
+            comic = client.first_series_matching(query_.title)
+            if not comic:
+                raise exceptions.NothingFound(f"Not found: {query_}")
+
+        logger.debug("Comic found: %s", comic)
+
+        chapter = [
+            chapter
+            for chapter in comic.chapters
+            if str(chapter.number) == str(query_.chapter)
+        ]
+        if not chapter:
+            raise exceptions.NothingFound(f"Chapter not found: {query_}")
+
+        chapter = chapter[0]
+
+        if query_.page > chapter.pages:
+            raise exceptions.NothingFound(
+                f"This chapter only has {chapter.pages} pages"
+            )
+
+        return cls(
+            uri=client.image_url(chapter.id, query_.page),
+            model=comic,
+            chapter_model=chapter,
+        )
+
+    def get_subtitles(self, *args, **kwargs):
+        raise exceptions.InvalidRequest("Quotes not supported for comics")
+
+    def get_frame(self, timestamps):
+        return utils.get_image_from_download_url(self._uri)
+
+    def register_post(self, post_id):
+        pass
+
+
+def _grammatically_join(words):
+    if len(words) == 0:
+        return ""
+    if len(words) == 1:
+        return words[0]
+
+    return f'{", ".join(words[:-1])} and {words[-1]}'
